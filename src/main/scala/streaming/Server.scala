a5ad51d84076c71da2cc4e4b1902a0a51c05832b
NonSvizzero <nonsvizzero@gmail.com>
2019-08-31 17:26:09 +0200

    Added post endpoint

--- src/main/scala/streaming/Server.scala
+++ src/main/scala/streaming/Server.scala
@@ -7,29 +7,42 @@ import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{Directives, Route}
+import streaming.Engine.readGraph
 import streaming.server.{JobDescription, JsonSupport}
+
 import scala.collection.JavaConverters._
 import scala.collection.immutable.HashMap
 import scala.collection.mutable.ListBuffer
 import scala.util.parsing.json.JSON
 
-object Server extends Directives with JsonSupport{
+object Server extends Directives with JsonSupport {
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem("server")
     implicit val materializer = ActorMaterializer()
-    var jobs = new HashMap[String, ActorRef]
+    var jobs = new HashMap[JobDescription, ActorRef]
 
     lazy val routes: Route =
       path("jobs") {
         get {
           var ls = new ConcurrentLinkedDeque[JobDescription]()
             jobs.foreach(job => {
-              ls.add(JobDescription(f"Job: ${job._1}"))
+              ls.add(job._1)
             })
           complete(ls.asScala)
         } ~
         post {
-          complete("Post endpoint")
+          entity(as[JobDescription]) { job =>
+            val jobName = job.name
+            val graphPath = job.path
+
+            val graph = readGraph(graphPath)
+            val newMasterNode: ActorRef = system.actorOf(MasterNode.props(() => graph), jobName)
+
+            val jobDescription = JobDescription(jobName, graphPath)
+            jobs = jobs.updated(jobDescription, newMasterNode)
+            newMasterNode ! MasterNode.CreateTopology
+            complete(jobDescription)
+          }
         } ~
         delete {
           complete("Delete endpoint")
--- src/main/scala/streaming/server/JobDescription.scala
+++ src/main/scala/streaming/server/JobDescription.scala
@@ -3,9 +3,9 @@ package streaming.server
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json.DefaultJsonProtocol
 
-case class JobDescription(name: String)
+case class JobDescription(name: String, path: String)
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
 
-  implicit val data = jsonFormat1(JobDescription)
+  implicit val data = jsonFormat2(JobDescription)
 }
